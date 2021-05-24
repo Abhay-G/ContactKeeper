b501574cd31dd9eb88bdc6a2be3f740fe2e42a5a
Abhay Gupta <abhaygupta15153986@gmail.com>
2021-05-24 13:09:02 +0530

    Auth Middleware and Protecting Routes

--- server.js
+++ server.js
@@ -1,11 +1,21 @@
 const express = require('express');
+const connectDB = require('./database/db');
+
 const app = express();
 
+//Init Middleware
+app.use(express.json({ extended: false }));
+
+//Home route
 app.get('/', (req, res) => res.json({ msg: 'Welcome to the Contact API' }));
 
-//DEFINE ROUTES
+//Define Routes
 app.use('/api/users', require('./routes/users'));
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/contacts', require('./routes/contacts'));
+
+////Connect Database and after that start server
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server started on ${PORT}`));
+connectDB().then(() =>
+    app.listen(PORT, () => console.log(`Server started on ${PORT}`))
+);
